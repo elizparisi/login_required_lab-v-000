845ab3b7f2e5d6151f9df0cf7123d742d7f1ecf9
Liz P <parisielizabeth@gmail.com>
2020-01-19 01:58:51 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  get 'secrets/new'
+  root 'application#welcome'
 
-  root 'application#hello'
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  post '/logout' => 'sessions#destroy'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
 
-  get '/secret' => 'secrets#show'
+  get '/the_secret_page', to: 'secrets#show'
 end
