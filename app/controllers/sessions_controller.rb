bd4a7db104b938d2229a3a6fdd34951c24878705
Liz P <parisielizabeth@gmail.com>
2020-01-19 01:18:23 +0000

    Automatically backed up by Learn

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  before_action :require_login
+  
+  def new 
+    
+  end 
+  
+  def create 
+    
+  end 
+  
+  def destroy
+    
+  end 
+end
