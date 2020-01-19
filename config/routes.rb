086b5dfefb42a3328bd7c7bac14a553afe35d02a
Liz P <parisielizabeth@gmail.com>
2020-01-19 01:34:34 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   get 'secrets/new'
 
   root 'application#hello'
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  post '/logout', to: 'sessions#destroy'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
 
-  get '/secret', to: 'secrets#show'
+  get '/secret' => 'secrets#show'
 end
